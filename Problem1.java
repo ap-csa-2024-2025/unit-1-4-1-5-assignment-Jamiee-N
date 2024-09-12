3d792b54d38b7352876a809ac3b0e1120282ff7e
Jamiee-N <jpnguyen124@student.hbuhsd.edu>
2024-09-12 11:20:50 -0700

    1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,10 @@
+public class Problem1
+{
+  public static void main(String[] args)
+  {
+    int num = 3;
+    num++;
+    System.out.print("number is now" + num);
+  }
+}
 
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,21 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    // problem 2
+    int number = 123;
+    int digit1 = (number % 10); //print 3
+    number = number/10; //make number 12
+
+    int digit2 = (number % 10); //print 2
+    number = number / 10; //make number 1
+
+    int digit3 = (number % 10); //print 1
+    number = number/10; //print number 0
+
+    System.out.println(digit3);
+    System.out.println(digit2);
+    System.out.print(digit1); // 1 2 3
+  }
+}
 
